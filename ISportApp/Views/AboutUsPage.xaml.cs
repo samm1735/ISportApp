b6a4352c046b068199e1c4be511e40a561872d27
samm1735 <ramclief17@gmail.com>
2024-04-30 15:22:19 -0400

    DONE

--- ISportApp/Views/AboutUsPage.xaml.cs
+++ ISportApp/Views/AboutUsPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class AboutUsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
+    private void OnGitHubButtonClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            
+            Launcher.OpenAsync(new Uri("https://github.com/samm1735"));
+        }
+        catch (Exception ex)
+        {
+            
+        }
+    }
+
 }
